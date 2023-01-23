2d92ab0fcb683519a9dc0876fa64a4eac9bb3c4b
Erki <erkiparna7@gmail.com>
2023-01-23 14:36:14 +0200

    Created connection to database

--- index.js
+++ index.js
@@ -6,8 +6,32 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = 3000;
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mysql://user:pass@localhost:PORT/joga_sequelize');
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connected to the database')
+    })
+    .catch(err => {
+        console.error('Unable to connect to database:', err);
+    });
+
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to sequelize application.' });
+});
+
+app.get('/article', (req, res) => {
+
+});
+
+app.get('/article/:slug', (req, res) => {
+
+});
+
+app.get('/author/:id', (req, res) => {
+
 });
 
 app.listen(PORT, () => {
