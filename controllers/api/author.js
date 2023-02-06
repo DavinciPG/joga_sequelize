a934ddc7c9ad3ff88b8b5987186f57aab304bfc8
Erki <erkiparna7@gmail.com>
2023-02-06 14:35:30 +0200

    Created API for author information by author name and id

---
+++ controllers/api/author.js
@@ -0,0 +1,51 @@
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize('mysql://user:pass@localhost:PORT/joga_sequelize');
+
+// read model data for table representation
+const models = require('../../models');
+
+// show author by name or id
+const getAuthor = (req, res) => {
+	let queryElement = Object.keys(req.query)
+	if(Object.keys(req.query).length === 0){
+		return res.status(400).json({ 'error': 'Invalid request'});
+	}else if(req.query.author_name === ''){
+		return res.status(400).json({ 'error': 'Invalid request'});
+	}else if(req.query.id === ''){
+		return res.status(400).json({ 'error': 'Invalid request'});
+	} else {
+	 if(queryElement[0] === 'author_name') {
+		models.Author.findOne({
+			where: {
+				name: req.query.author_name
+			},
+			include: [{
+				model: models.Article
+			}],
+		})
+		.then(article => {
+			return res.status(200).json({ article });
+		})
+		.catch (error => {
+			return res.status(500).send(error.message);
+		})
+	} if(queryElement[0] === 'id') {
+		models.Author.findByPk(req.query.id, {
+		include: [{
+			model: models.Article
+		}],
+	})
+	.then(author => {
+		return res.status(200).json({ author });
+	})
+	.catch (error => {
+		return res.status(500).send(error.message);
+	})
+	}
+	}
+};
+
+// export
+module.exports = {
+	getAuthor
+}
--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+
 const apiArticleController = require('../controllers/api/article');
+const apiAuthorController = require('../controllers/api/author');
 
 router.get('/article', apiArticleController.getArticle);
+router.get('/author', apiAuthorController.getAuthor);
 
 module.exports = router;
